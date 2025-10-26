db0404c7716b2b13d130c91ba13c30be166ea4c1
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2025-10-26 15:09:05 +0530

    Add ServerConstants class with configuration values

---
+++ server.java
@@ -0,0 +1,7 @@
+package server;
+
+public class ServerConstants {
+    public static final int SERVER_PORT = 12345;
+    public static final String SERVER_HOST = "localhost";
+    public static final int MAX_CLIENTS = 50;
+}
